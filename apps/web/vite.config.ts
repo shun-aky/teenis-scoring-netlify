ebb8892e6c1ca7e38ffa6827b8a27c863b1ac4bd
shunaky <shunaky@umich.edu>
2025-09-10 23:25:18 -0400

    Updated apps/web/vite.config.ts so that @tennis/core is recognized

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,13 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@tennis/core': path.resolve(__dirname, '../../packages/core/src'),
+    },
+  },
   server: { port: 5173 }
 })
